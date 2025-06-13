bd64c75dd24d01a6022e9aa6b0909d56521a79fb
okhsunrog <me@okhsunrog.dev>
2025-06-13 03:05:02 +0300

    trying to add spi + display

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,11 +14,29 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{
     Async,
+    dma::{DmaRxBuf, DmaTxBuf},
     clock::CpuClock,
     i2c::master::{Config as I2cConfig, Error as I2cError, I2c},
     time::Rate,
     timer::timg::TimerGroup,
+    spi::{
+        master::{Config, Spi, SpiDmaBus},
+        Mode,
+    },
 };
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
+use mipidsi::{interface::SpiInterface, models::ST7789, options::ColorInversion, raw_framebuf::RawFrameBuf, Builder};
+use embedded_hal::digital::OutputPin;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_10X20, MonoTextStyle},
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{PrimitiveStyle, Rectangle},
+    text::{Alignment, Text},
+};
+
+use static_cell::StaticCell;
 
 use esp_wifi::ble::controller::BleConnector;
 use {esp_backtrace as _, esp_println as _};
@@ -29,12 +47,21 @@ extern crate alloc;
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
+
+const W: u16 = 135;
+const H: u16 = 240;
+const X_OFFSET: u16 = 0;
+const Y_OFFSET: u16 = 0;
+const W_ACTIVE: usize = (W - X_OFFSET) as usize; // 135
+const H_ACTIVE: usize = (H - Y_OFFSET) as usize; // 240
+const PXL_SIZE: usize = 2;
+
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let p = esp_hal::init(config);
-
-    esp_alloc::heap_allocator!(size: 64 * 1024);
+    esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 64000);
 
     let timer0 = TimerGroup::new(p.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
@@ -60,6 +87,45 @@ async fn main(spawner: Spawner) {
     // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
     let transport = BleConnector::new(&wifi_init, p.BT);
     let _ble_controller = ExternalController::<_, 20>::new(transport);
+
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = esp_hal::dma_buffers!(4, 32_000);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+    // Creating SPI
+    let sclk = p.GPIO13;
+    let mosi = p.GPIO15;
+    let res = p.GPIO18;
+    let dc = p.GPIO23;
+    let cs = p.GPIO5;
+    let spi = Spi::new(
+        p.SPI2,
+        Config::default().with_frequency(Rate::from_mhz(20)).with_mode(Mode::_0),
+    )
+    .unwrap()
+    .with_sck(sclk)
+    .with_mosi(mosi)
+    .with_dma(p.DMA_SPI2)
+    .with_buffers(dma_rx_buf, dma_tx_buf)
+    .into_async();
+
+    static SPI_BUS: StaticCell<Mutex<NoopRawMutex, SpiDmaBus<'static, Async>>> = StaticCell::new();
+    let spi_bus = Mutex::new(spi);
+    let spi_bus = SPI_BUS.init(spi_bus);
+    let spi_device = SpiDevice::new(spi_bus, cs);
+
+    let di = SpiInterface::new(spi_device, dc);
+    let mut delay = embassy_time::Delay;
+
+    let mut display = Builder::new(ST7789, di)
+        .reset_pin(res)
+        .display_size(W_ACTIVE, H_ACTIVE)
+        .display_offset(X_OFFSET, Y_OFFSET)
+        .invert_colors(ColorInversion::Inverted)
+        .init(&mut delay)
+        .await
+        .unwrap();
+    info!("Display initialized!");
 
     spawner.must_spawn(pd_task(i2c0));
 
